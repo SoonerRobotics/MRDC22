c910ef896d41c730c27a5e2b0aa80767f17dfe82
Dylan Zemlin <dylansmrw@gmail.com>
2021-11-02 21:09:49 -0500

    Added resource file to mrdc_joy setup

--- mrdc_ws/src/mrdc_joy/setup.py
+++ mrdc_ws/src/mrdc_joy/setup.py
@@ -7,6 +7,7 @@ setup(
     version='1.0.0',
     packages=find_packages(),
     data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name, ['launch/all_launch.xml'])
     ],
