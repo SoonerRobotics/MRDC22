ee22d4f2b4e647ba6fa3489b7cff0bc4e5a34a06
Dylan Zemlin <dylansmrw@gmail.com>
2021-11-02 21:09:01 -0500

    Added mrdc_core package

---
+++ mrdc_ws/src/mrdc_core/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+package_name = 'mrdc_core'
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=find_packages(),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['launch/all_launch.xml'])
+    ],
+    install_requires=['setuptools'],
+    maintainer='Dylan Zemlin',
+    maintainer_email='dylansmrw@gmail.com',
+    description='TODO: Package description',
+    license='Apache License 2.0',
+    entry_points={
+        'console_scripts': [
+            'core = src.core:main'
+        ],
+    },
+)
