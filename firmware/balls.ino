a52035c16b8c3064c09d26108ebc06c270916879
Dylan Zemlin <dylansmrw@gmail.com>
2022-03-22 19:51:09 -0500

    Updated firmware

---
+++ firmware/balls.ino
@@ -0,0 +1,46 @@
+#include <RobotLib.h>
+#include <ardubson.h>
+
+Motor leftMotor;
+Motor rightMotor;
+
+BSONStreamParser streamParser;
+
+void messageHandler(BSONObject* bson_obj);
+
+int intake = 0;
+int elevator = 0;
+int launcher = 0;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  streamParser.setMessageHandler(messageHandler);
+}
+
+void messageHandler(BSONObject * bson_obj) {
+  uint16_t cmd = bson_obj->getField("cmd").getInt();
+
+  if (cmd == 0x01) {
+    elevator = bson_obj->getField("elevator_motor").getInt();
+    intake = bson_obj->getField("intake_motor").getInt();
+    launcher = bson_obj->getField("launcher_motor").getInt();
+  }
+}
+
+void loop()
+{
+  processSerialInput();
+
+  leftMotor.output(left);
+  rightMotor.output(right);
+}
+
+void processSerialInput() {
+  while (Serial.available()) {
+    char c = Serial.read();
+    streamParser.analizeIncomingChars(c);
+  }
+  streamParser.update();
+}
---
+++ firmware/drivetrain.ino
@@ -0,0 +1,55 @@
+#include <RobotLib.h>
+#include <ardubson.h>
+
+Motor leftMotor;
+Motor rightMotor;
+
+BSONStreamParser streamParser;
+
+void messageHandler(BSONObject* bson_obj);
+
+int left = 0;
+int right = 0;
+
+void setup()
+{
+  leftMotor.begin(4, 5, 6);
+  rightMotor.begin(7, 8, 9);
+
+  Serial.begin(115200);
+
+  streamParser.setMessageHandler(messageHandler);
+}
+
+void messageHandler(BSONObject * bson_obj) {
+  uint16_t cmd = bson_obj->getField("cmd").getInt();
+
+  if (cmd == 0x01) {
+    left = bson_obj->getField("left_motor").getInt();
+    right = bson_obj->getField("right_motor").getInt();
+  }
+}
+
+void loop()
+{
+  processSerialInput();
+
+ if (left < 0.1 && left > -0.1) {
+   left = 0;
+ }
+
+ if (right < 0.1 && right > -0.1) {
+   right = 0;
+ }
+
+  leftMotor.output(left);
+  rightMotor.output(right);
+}
+
+void processSerialInput() {
+  while (Serial.available()) {
+    char c = Serial.read();
+    streamParser.analizeIncomingChars(c);
+  }
+  streamParser.update();
+}
